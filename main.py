0b681352d962f0936502688ae4d231de3fba576b
jeremy <je.gautier137@gmail.com>
2021-02-28 19:26:37 +0100

    Version 1.2

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
 import re
 
-#Version 1.0 - 01/18/2020, plain text - version 1.2 planned with reading a text file, processing as
-#than rewriting the cleaned strings in another text file.
+#Version 1.2 - 28/02/2021.
 
-#Initialize text
-txt = 'fdfs,"TheraininSpain",dsfddf,slfsfsld,"dfddfsg",salut,"toi"'
+#reading file txt
 
-#sort by REGEX
-x = re.findall(r"(?<=\")\w+", txt)
+file = open("test.txt", "r")
 
-#Displaying x with a line break
-print("\n".join(x))
+#regex on file
+x = re.findall(r"(?<=\")\w+[\w\-\?\-\/\.\=\&\;\$\:\+\!\%\@]*", file.read())
+file.close()
+
+#carriage return
+y = "\n".join(x)
+
+#create a new file and write
+fichier = open("data.txt", "a")
+fichier.write(y)
+fichier.close()
+
+
+
+
 
 
